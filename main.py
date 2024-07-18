ab093aca86f7321d4a35b6765c94a96362e57b68
piop2 <pioooop2@gmail.com>
2024-07-18 09:56:39 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+class Main:
+    def __init__(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def run(self) -> None:
+        while True:
+            self.update()
+
+
+if __name__ == "__main__":
+    Main().run()
